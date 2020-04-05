8e1735d2f5d495ee99f2f7d9475f6488821e071c
ThuF <jordan.p.pavlov@gmail.com>
2020-04-05 18:00:00 +0300

    Adds "api-modules" Extension

---
+++ db/extensions/modules.js
@@ -0,0 +1,27 @@
+exports.getContent = function() {
+	return [{
+		name: "db/v4/dao",
+		description: "Database DAO API"
+	}, {
+		name: "db/v4/database",
+		description: "Database API"
+	}, {
+		name: "db/v4/orm",
+		description: "Database ORM API"
+	}, {
+		name: "db/v4/ormstatements",
+		description: "Database ORM Statements API"
+	}, {
+		name: "db/v4/query",
+		description: "Database Query API"
+	}, {
+		name: "db/v4/sequence",
+		description: "Database Sequence API"
+	}, {
+		name: "db/v4/sql",
+		description: "SQL API"
+	}, {
+		name: "db/v4/update",
+		description: "Database Update API"
+	}];
+};
