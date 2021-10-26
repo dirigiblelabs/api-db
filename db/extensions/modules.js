c69e77b57e0574a44ef9a577dcd8f24273b627fc
Ivan <iwolkkov@gmail.com>
2021-10-26 09:47:47 +0300

    Added d.ts for module.

--- db/extensions/modules.js
+++ db/extensions/modules.js
@@ -1,41 +1,17 @@
 /*
- * Copyright (c) 2010-2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
- * SPDX-FileCopyrightText: 2010-2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  * SPDX-License-Identifier: EPL-2.0
  */
-exports.getContent = function() {
-	return [{
-		name: "db/v4/dao",
-		description: "Database DAO API"
-	}, {
-		name: "db/v4/database",
-		description: "Database API"
-	}, {
-		name: "db/v4/orm",
-		description: "Database ORM API"
-	}, {
-		name: "db/v4/ormstatements",
-		description: "Database ORM Statements API"
-	}, {
-		name: "db/v4/procedure",
-		description: "Database Procedure API"
-	}, {
-		name: "db/v4/query",
-		description: "Database Query API"
-	}, {
-		name: "db/v4/sequence",
-		description: "Database Sequence API"
-	}, {
-		name: "db/v4/sql",
-		description: "SQL API"
-	}, {
-		name: "db/v4/update",
-		description: "Database Update API"
-	}];
+var registry = require("platform/v4/registry")
+exports.getContent = function () {
+    let file = registry.getText("db/extensions/modules.json");
+    return JSON.parse(file);
+
 };
